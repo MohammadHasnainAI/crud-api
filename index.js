bb852d87439f1e1a142315d4def34c2aba38af61
Mohammad Hasnain <hasnain.mohammad2003@gmail.com>
2026-07-14 19:56:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ app.delete("/todos/:id", (req, res) => {
   res.json({ message: "Todo deleted" });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
